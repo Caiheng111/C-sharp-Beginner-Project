4f58ec032959de88426cf4c9d2d841d6f237c162
Caiheng111 <ianyaheng822@gmail.com>
2024-03-05 17:23:36 +1100

    add quize project

--- Projects.cs
+++ Projects.cs
@@ -1,4 +1,9 @@
-﻿namespace CSharp;
+﻿
+
+using System.Runtime.Intrinsics.X86;
+
+using System.IO;
+
 
 class Program
 {
@@ -75,9 +80,60 @@ class Program
 
         Console.ReadKey();
     }
+
+    static void Quize()
+    {
+        string[] text = File.ReadAllLines("questions.txt");
+        List<string> questions = new List<string>();
+        List<string> answers = new List<string>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (i % 4 == 0)
+                questions.Add(text[i]);
+            else
+                answers.Add(text[i]);
+        }
+
+        int questionIndex = 0;
+        int answerIndex = 0;
+        int score = 0;
+
+        while (questionIndex < questions.Count)
+        {
+            Console.WriteLine("Question: " + questions[questionIndex]);
+            questionIndex++;
+
+            int correctAnswer = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (answers[answerIndex].StartsWith(">"))
+                    correctAnswer = i + 1;
+
+                Console.WriteLine(i + 1 + "." + answers[answerIndex].Replace(">", ""));
+
+                answerIndex++;
+            }
+
+            int answer = int.Parse(Console.ReadLine());
+
+            if (answer == correctAnswer)
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+                Console.WriteLine("Incorrect.");
+        }
+
+        Console.WriteLine("End of quiz! Your score was " + score + " points!");
+        Console.ReadKey();
+    }
     static void Main(string[] args) {
-        ShapeAreaCalculator();
-        Calculator();
+        //ShapeAreaCalculator();
+        //Calculator();
+        Quize();
 
     }  
 }
